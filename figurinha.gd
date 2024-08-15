87cebef6e79535742b8237cb083107d086eef98a
Matheus Herzog <110929706+MatheuzH@users.noreply.github.com>
2024-08-15 20:14:17 -0300

    Add files via upload

---
+++ figurinha.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+@export var velocidade = 2
+
+
+func minha_func():
+	position.y += velocidade
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position.x = 538
+	position.y = 300
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	minha_func()
