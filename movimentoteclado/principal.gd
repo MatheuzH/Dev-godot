94fd69c1f3a2680da2203af7fc4f0790d00956cc
Matheus Herzog <110929706+MatheuzH@users.noreply.github.com>
2024-08-22 21:18:17 -0300

    Add files via upload

---
+++ movimentoteclado/principal.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var numero_magico = 3 
+var imprimiu = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print_debug("Comeuçou o jogo")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if numero_magico > 5 and not imprimiu:
+		print_debug("Múmero mágico aumentou")
+		imprimiu = true 
